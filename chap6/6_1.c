d9bf5bb2ba0f051cfc08b3f1c1e3f193040fe37e
Twaha Rahman <mahi6703890@gmail.com>
2023-12-24 11:13:25 +0600

    Fixed the leap year bug in 6_1.c code

--- chap6/6_1.c
+++ chap6/6_1.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 int is_date_valid(char *date_string, int string_len);
+int is_leap_year(int year);
 
 int main() {
   char date_string[11];
@@ -14,7 +15,7 @@ int main() {
   if (is_date_valid(date_string, read_char_count)) {
     printf("\nThe provided date is valid");
   } else {
-    printf("\nThe provided date is invalid");
+    printf("\nThe provided date is NOT invalid");
   }
 
   return 0;
@@ -53,8 +54,14 @@ int is_date_valid(char *date_string, int string_len) {
     return 0;
   }
 
-  // TODO: Leap year check and adjust February's day count
+  // The number of days in a given month
   int days_in_month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+  // If the year is a leap year, then set the
+  // number of days in February to be 29 days
+  if (is_leap_year(year)) {
+    days_in_month[1] = 29;
+  }
 
   if (day < 1 || day > days_in_month[month - 1]) {
     return 0;
@@ -66,4 +73,13 @@ int is_date_valid(char *date_string, int string_len) {
   }
 
   return 1;
+}
+
+// If the year is a leap year, returns 1. Otherwise, the function returns 0
+int is_leap_year(int year) {
+  if ((year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0))) {
+    return 1;
+  }
+
+  return 0;
 }
