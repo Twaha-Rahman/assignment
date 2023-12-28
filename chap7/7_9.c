a802733eec60061fefa5e6786be56de483486f4e
Twaha Rahman <mahi6703890@gmail.com>
2023-12-28 12:48:34 +0600

    Fix the bug due to my misunderstanding

--- chap7/7_9.c
+++ chap7/7_9.c
@@ -3,16 +3,21 @@
 int main() {
   // get user input for the number of terms to sum up
   int n;
-  printf("Enter the number of terms to sum up to: ");
+  printf("The number of terms to sum up (cannot be less than 2): ");
   scanf("%d", &n);
 
+  if (n < 2) {
+    printf("\nPlease enter a value greater than 2");
+    return 0;
+  }
+
   // the variable to store the sum
-  double result = 0.0;
+  double result = (1.0 / 3.0) + (3.0 / 7.0);
 
   // set the variables for the initial fraction
-  int numerator = 1;
-  int denominator = 3;
-  double sign = 1.0;
+  int numerator = 5;
+  int denominator = 11;
+  double sign = -1.0;
 
   // calculate the sum upto the n-th term
   for (int i = 3; i <= n; i++) {
@@ -23,7 +28,7 @@ int main() {
   }
 
   // print the sum
-  printf("Sum of the sequence upto the %d-th term: %.2f\n", n, result);
+  printf("\nSum of the sequence upto the %d-th term: %.2f\n", n, result);
 
   return 0;
 }
