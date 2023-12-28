a7d7d8746348b01b20e55699aa176fa45b008758
Twaha Rahman <mahi6703890@gmail.com>
2023-12-28 12:36:19 +0600

    Fix the code for the problem 7.9

--- chap7/7_9.c
+++ chap7/7_9.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 
 int main() {
-  // calculate the sum of the series
-  double result = (1.0 / 3.0) + (3.0 / 7.0) - (5.0 / 11.0) + (7.0 / 15.0);
+  // get user input for the number of terms to sum up
+  int n;
+  printf("Enter the number of terms to sum up to: ");
+  scanf("%d", &n);
+
+  // the variable to store the sum
+  double result = 0.0;
+
+  // set the variables for the initial fraction
+  int numerator = 1;
+  int denominator = 3;
+  double sign = 1.0;
+
+  // calculate the sum upto the n-th term
+  for (int i = 3; i <= n; i++) {
+    result += (sign * numerator) / denominator;
+    numerator += 2;
+    denominator += 4;
+    sign *= -1.0;
+  }
 
   // print the sum
-  printf("\nThe result is: %lf", result);
+  printf("Sum of the sequence upto the %d-th term: %.2f\n", n, result);
 
   return 0;
 }
